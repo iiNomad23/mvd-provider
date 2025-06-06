65f22df46ea912856ca3dbc0cee981b74b9fee7a
Michael Spiegel <70846025+Smightym8@users.noreply.github.com>
2025-06-06 15:40:19 +0200

    Enhance logging for OrganizationLocationFunction

--- extensions/dcp-impl/src/main/java/org/eclipse/edc/demo/dcp/policy/OrganizationLocationFunction.java
+++ extensions/dcp-impl/src/main/java/org/eclipse/edc/demo/dcp/policy/OrganizationLocationFunction.java
@@ -4,44 +4,55 @@ import org.eclipse.edc.participant.spi.ParticipantAgentPolicyContext;
 import org.eclipse.edc.policy.engine.spi.AtomicConstraintRuleFunction;
 import org.eclipse.edc.policy.model.Operator;
 import org.eclipse.edc.policy.model.Permission;
+import org.eclipse.edc.spi.monitor.Monitor;
 
 import java.util.Map;
 
 import static org.eclipse.edc.demo.dcp.policy.PolicyEvaluationExtension.MEMBERSHIP_CONSTRAINT_KEY;
 
-// TODO: Print logs
 public class OrganizationLocationFunction<C extends ParticipantAgentPolicyContext> extends AbstractCredentialEvaluationFunction implements AtomicConstraintRuleFunction<Permission, C> {
     private static final String ORGANIZATION_CLAIM = "organization";
     private static final String LOCATION_CLAIM = "location";
 
-    private OrganizationLocationFunction() {}
+    private final Monitor monitor;
 
-    public static <C extends ParticipantAgentPolicyContext> OrganizationLocationFunction<C> create() {
-        return new OrganizationLocationFunction<>() {
+    private OrganizationLocationFunction(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public static <C extends ParticipantAgentPolicyContext> OrganizationLocationFunction<C> create(Monitor monitor) {
+        return new OrganizationLocationFunction<>(monitor) {
         };
     }
 
     @Override
     public boolean evaluate(Operator operator, Object rightValue, Permission permission, ParticipantAgentPolicyContext  policyContext) {
         if (!(rightValue instanceof String)) {
-            policyContext.reportProblem("Right-value expected to be String but was " + rightValue.getClass());
+            var message = "Right-value expected to be String but was " + rightValue.getClass();
+            policyContext.reportProblem(message);
+            monitor.debug(message);
             return false;
         }
 
         if (operator != Operator.EQ) {
-            policyContext.reportProblem("Invalid operator, only EQ is allowed!");
+            var message = "Invalid operator, only EQ is allowed!";
+            policyContext.reportProblem(message);
+            monitor.debug(message);
             return false;
         }
 
         var participantAgent  = policyContext.participantAgent();
         if (participantAgent  == null) {
-            policyContext.reportProblem("ParticipantAgent not found on PolicyContext");
+            var message = "ParticipantAgent not found on PolicyContext";
+            policyContext.reportProblem(message);
+            monitor.debug(message);
             return false;
         }
 
         var credentialResult = getCredentialList(participantAgent);
         if (credentialResult.failed()) {
             policyContext.reportProblem(credentialResult.getFailureDetail());
+            monitor.debug(credentialResult.getFailureDetail());
             return false;
         }
 
@@ -55,7 +66,9 @@ public class OrganizationLocationFunction<C extends ParticipantAgentPolicyContex
                     var isLocationEqual = locationClaim.equalsIgnoreCase(rightValue.toString());
 
                     if (!isLocationEqual) {
-                        policyContext.reportProblem("Location expected to be '%s' but was '%s'".formatted(rightValue.toString(), locationClaim));
+                        var message = "Location expected to be '%s' but was '%s'".formatted(rightValue.toString(), locationClaim);
+                        policyContext.reportProblem(message);
+                        monitor.debug(message);
                     }
 
                     return isLocationEqual;
